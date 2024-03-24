8d034f945b6131a7444f751e84e1aafd2361ea32
Matias Lapolla <matias.lapolla.1@gmail.com>
2024-03-24 16:16:40 -0300

    add cors and usage limit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,16 @@
 import express, { Express } from "express";
 import { router } from "../router/router";
+var cors = require("cors");
 
+const corsOptions = { credentials: true, origin: true };
 const app: Express = express();
 
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
 app.set("port", process.env.PORT || 3003);
 app.set("host", process.env.HOST || "localhost");
 app.set("env", process.env.NODE_ENV || "development");
 app.set("json spaces", 2);
 app.set("trust proxy", true);
-
+app.use(cors({ origin: "*" }));
 app.use("/api", router);
 export { app };
